c9526096e813f07c733aece2303b0eb14737b34e
jntellez <juantellez916@gmail.com>
2026-03-28 01:38:05 -0600

    fix(Badge): correct font weight in Badge component for improved readability

--- packages/glass/src/ui/badge.tsx
+++ packages/glass/src/ui/badge.tsx
@@ -11,7 +11,7 @@ const Badge = React.forwardRef<HTMLSpanElement, React.HTMLAttributes<HTMLSpanEle
       <span
         ref={ref}
         className={cn(
-          "inline-flex items-center h-fit rounded-full text-xs font-medium transition-all duration-200",
+          "inline-flex items-center h-fit rounded-full text-xs font-normal transition-all duration-200",
           "px-2.5 py-0.5",
           "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black/20 dark:focus-visible:ring-white/50 focus-visible:ring-offset-2",
           !hasCustomSurface && [
